45a785b63c48a9309919763517d218b4fe7fafa9
theotheo46 <theotheo46@gmail.com>
2024-03-11 18:22:06 +0300

    go

---
+++ 1/4/filter.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func filter(predicate func(int) bool, iterable []int) []int {
+	filtered := make([]int, 0)
+
+	for _, v := range iterable {
+		if predicate(v) {
+			filtered = append(filtered, v)
+		}
+	}
+	return filtered
+}
+
+func main() {
+	src := readInput()
+	res := filter(func(i int) bool { return i%2 == 0 }, src)
+	fmt.Println(res)
+}
+
+// ┌─────────────────────────────────┐
+// │ не меняйте код ниже этой строки │
+// └─────────────────────────────────┘
+
+// readInput считывает целые числа из `os.Stdin`
+// и возвращает в виде среза
+// разделителем чисел считается пробел
+func readInput() []int {
+	var nums []int
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Split(bufio.ScanWords)
+	for scanner.Scan() {
+		num, _ := strconv.Atoi(scanner.Text())
+		nums = append(nums, num)
+	}
+	return nums
+}
